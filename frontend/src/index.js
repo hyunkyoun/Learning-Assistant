a5fec98af8440c50fc1c2766d8c04a14f0a3e8ea
hyunkyoun <ekim2350@gmail.com>
2024-10-26 20:28:31 -0400

    created quiz

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Components/Login'
+import Login from './Components/Login';
+import Quiz from './Components/Quiz/Quiz';
 import reportWebVitals from './reportWebVitals';
 import Main from './Components/ClassLayout/MainLayout/MainLayout';
 
@@ -10,7 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Login />
+    {/* <Login /> */}
+    <Quiz />
   </React.StrictMode>
 );
 
