1ec8f5eae9f2d9eaa26b5e1e9312fa8673433933
hyunkyoun <ekim2350@gmail.com>
2024-10-27 02:53:56 -0400

    backend auth routes

--- backend/server.js
+++ backend/server.js
@@ -2,50 +2,48 @@ const express = require('express');
 const mongoose = require("mongoose");
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
+
+const { port, uri } = require('./config.json');
 
 const conversationRoute = require("./routes/Conversations");
 // const messageRoute = require("./routes/messages");
+const authRoutes = require('./routes/authRoutes');
+
 const router = express.Router();
 const path = require("path");
 
-mongoose.connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log("Connected to MongoDB");
-    }
-);
-
 const app = express();
-const PORT = process.env.PORT || 5001;
-const authRoutes = require('./routes/authRoutes');
+const PORT = port || 5001;
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "public/images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, "public/images");
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, req.body.name);
+//     },
+// });
   
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("File uploded successfully");
-  } catch (error) {
-    console.error(error);
-  }
-});
+// const upload = multer({ storage: storage });
+// app.post("/api/upload", upload.single("file"), (req, res) => {
+//   try {
+//     return res.status(200).json("File uploded successfully");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api', authRoutes);
-app.use("/api", conversationRoute);
-app.use("/api", messageRoute);
+// app.use("/api", conversationRoute);
+// app.use("/api", messageRoute);
+
+mongoose.connect(uri)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`Backend server is running!`);
