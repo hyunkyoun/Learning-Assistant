aed579c1138ffb7537239128d1e7656f14f0177b
hyunkyoun <ekim2350@gmail.com>
2024-10-27 02:33:07 -0400

    .

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const conversationRoute = require("./routes/Conversations");
-const messageRoute = require("./routes/messages");
+// const messageRoute = require("./routes/messages");
 const router = express.Router();
 const path = require("path");
 
 mongoose.connect(
-    process.env.MONGO_URL,
+    process.env.MONGO_URI,
     { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
       console.log("Connected to MongoDB");
